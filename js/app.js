ee70d40cd97856c00386935c11abe8506949aaf6
joymohanta <joymohanta06@gmail.com>
2022-03-02 05:58:08 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -34,4 +34,17 @@ const mobileDetails = (slug) => {
 };
 const displayDetails = (phone) => {
   console.log(phone);
+  const detailItem = document.getElementById("cards");
+  detailItem.textContent = "";
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.innerHTML = `
+  <img src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  `;
+  detailItem.appendChild(div);
 };
