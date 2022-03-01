6130a7920ddf5168b24e90203a7c3794d70a9cc9
joymohanta <joymohanta06@gmail.com>
2022-03-02 05:25:01 +0600

    second commit

--- js/app.js
+++ js/app.js
@@ -16,13 +16,22 @@ const displayPhone = (phones) => {
     div.classList.add("col");
     div.innerHTML = `
     <div class="card h-100">
-      <img src="${phone.image}" class="card-img-top" alt="...">
+      <img src="${phone.image}" class="card-img-top size" alt="...">
       <div class="card-body">
         <h5 class="card-title">${phone.brand}</h5>
-        <h5 class="card-title">${phone.phone_name}</h5>
+        <h5 class="card-title part">${phone.phone_name} <button onclick="mobileDetails('${phone.slug}')">Explore</button></h5>
       </div>
     </div>
       `;
     containerDiv.appendChild(div);
   });
+};
+const mobileDetails = (slug) => {
+  console.log(slug);
+  fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+    .then((res) => res.json())
+    .then((data) => displayDetails(data.data));
+};
+const displayDetails = (phone) => {
+  console.log(phone);
 };
