244309c6926ba3253854b6fa34d739f9140a6f15
joymohanta <joymohanta06@gmail.com>
2022-03-02 15:10:06 +0600

    almost done

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
+// Input Search Field
 const searchPhone = () => {
   const inputPhone = document.getElementById("input-field");
   const inputText = inputPhone.value;
   inputPhone.value = "";
-  //   console.log(inputText);
-  fetch(`https://openapi.programming-hero.com/api/phones?search=${inputText}`)
-    .then((res) => res.json())
-    .then((data) => displayPhone(data.data));
+  if (inputText == "") {
+    const error = document.getElementById("error");
+    error.style.display = "block";
+  } else {
+    //   console.log(inputText);
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${inputText}`)
+      .then((res) => res.json())
+      .then((data) => displayPhone(data.data));
+    const error = document.getElementById("error");
+    error.style.display = "none";
+  }
 };
+// UI Card Show
 const displayPhone = (phones) => {
-  console.log(phones);
+  // console.log(phones);
   const containerDiv = document.getElementById("container");
   containerDiv.textContent = "";
   phones.forEach((phone) => {
@@ -19,21 +28,23 @@ const displayPhone = (phones) => {
       <img src="${phone.image}" class="card-img-top size" alt="...">
       <div class="card-body">
         <h5 class="card-title">${phone.brand}</h5>
-        <h5 class="card-title part">${phone.phone_name} <button onclick="mobileDetails('${phone.slug}')">Explore</button></h5>
+        <h5 class="card-title part">${phone.phone_name} <button onclick="mobileDetails('${phone.slug}')" class = "btn">Explore</button></h5>
       </div>
     </div>
       `;
     containerDiv.appendChild(div);
   });
 };
+// Card Button Section
 const mobileDetails = (slug) => {
-  console.log(slug);
+  // console.log(slug);
   fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
     .then((res) => res.json())
     .then((data) => displayDetails(data.data));
 };
+// Device Details Section
 const displayDetails = (phone) => {
-  console.log(phone);
+  // console.log(phone);
   const detailItem = document.getElementById("cards");
   detailItem.textContent = "";
   const div = document.createElement("div");
@@ -51,8 +62,6 @@ const displayDetails = (phone) => {
               <p class="card-text"><small>Chipset: ${phone.mainFeatures.chipSet}</small></p>
               <p class="card-text"><small>Memory: ${phone.mainFeatures.memory}</small></p>
               <p><small>Storage: ${phone.mainFeatures.storage}</small></p>
-              
-              
             </div>
           </div>
         </div>
